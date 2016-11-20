2ddbdb237b7ee1c7e52f7c883918d0614f2b8a9f
PTomi002 <tamas.paulin91@gmail.com>
2016-11-20 13:53:21 +0100

    LDAp database clean script
    Change-Id: Idbf8012a4a68130934b16b51c735b5283fd91837

--- TRHandlerTool/src/main/resources/scripts/ldap_clean.sh
+++ TRHandlerTool/src/main/resources/scripts/ldap_clean.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-echo "Hello World!"
+#########################################
+# Variables
+#########################################
+BASE_DN="ou=People,dc=onlab,dc=msc,dc=bme,dc=hu"
+UID_DN="cn=Users,ou=Groups,dc=onlab,dc=msc,dc=bme,dc=hu"
+ENTRIES=""
+METHOD=1
+PWD=`pwd`
+
+#########################################
+# Main
+#########################################
+echo "($METHOD) Searching for entries to delete."
+
+ENTRIES=`ldapsearch -H "ldap://localhost:389" -D "cn=admin,dc=onlab,dc=msc,dc=bme,dc=hu" \
+	-w "rootroot" -b $BASE_DN -s children \
+	| grep -i "dn" | awk '{split($0,UID," "); print UID[2]}'`
+
+echo "Search finished, found entries:"
+
+if [[ ! -z "${ENTRIES}" ]]
+then
+	echo $ENTRIES
+	METHOD=$((METHOD+1))
+	echo "($METHOD) Deleting found entries."
+	ldapdelete -H "ldap://localhost:389" -D "cn=admin,dc=onlab,dc=msc,dc=bme,dc=hu" \
+		-c -v -w "rootroot" $ENTRIES
+	echo "Done: Entries has been deleted!"
+else
+	echo "There are no entries to delete!"
+fi
+
+METHOD=$((METHOD+1)) 
+echo "($METHOD) Deleting uid attributes from: $UID_DN"
+
+ldapmodify -H "ldap://localhost:389" -D "cn=admin,dc=onlab,dc=msc,dc=bme,dc=hu" \
+	-w "rootroot" -v -f $PWD/src/main/resources/scripts/ldapmodify.ldif
+
+echo "DONE, exiting..."
+exit 0;
